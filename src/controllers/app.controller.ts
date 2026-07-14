2ec59d8b5bcb0954682dd101a3a1e06045926b1b
Lyubomir Petkov <lpetkov44@yahoo.com>
2026-07-15 00:43:14 +0300

    Remove unused dependencies and normalise line endings to LF

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,20 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from '../services/app.service';
-import { fsReadFile } from 'ts-loader/dist/utils';
-import { Abilities } from '../entities/abilities';
-import { AbilitiesService } from '../services/abilities.service';
 
 @Controller()
 export class AppController {
-  constructor() {}
-
   @Get()
-  async getHello(): Promise<any> {
-    // this.appService.getHello();
-    // cd('/data');
-    // exec('git clone https://github.com/BSData/wh40k-10e.git');
-    const res = '';
-
+  async getHello(): Promise<string> {
     return '';
   }
 }
--- src/services/datasheetsModelsCost.service.ts
+++ src/services/datasheetsModelsCost.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { BaseService } from './base.service';
 import { DatasheetsModelsCost } from '../entities/datasheetsModelsCost';
-import { Datasheets } from '../entities/datasheets';
 
 @Injectable()
 export class DatasheetsModelsCostService extends BaseService(
